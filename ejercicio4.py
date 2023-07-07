80e0de0b5fd4e50b655cb52352a7abcc923b3b95
ropaez2001 <ropaez2111@gmail.com>
2023-07-07 20:10:44 -0300

    Prueba

---
+++ ejercicio4.py
@@ -0,0 +1,11 @@
+numero1=int(input( " ingresar el primer numero: "))
+numero2=int(input( " ingresar el segundo numero :" ))
+while 
+
+
+if numero1 > numero2:
+     print("***** El segundo numero es menor es el menos ****")
+elif numero2 > numero1:
+    print("***** El primer numero es menor que el segundo ****")
+else:
+    print(" ***** Los numeros ingresados son iguales *******")
